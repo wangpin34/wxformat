9f78c72ce852a574a056319ed86ea18855ec30af
pin-wang <pin.wang@hp.com>
2021-12-15 17:28:58 +0800

    add comments

--- src/states/id.ts
+++ src/states/id.ts
@@ -5,6 +5,18 @@ import { nanoid } from 'nanoid'
 
 const rendererState = atom<string>({
   key: 'rendererState',
+  /**
+   * In the begining, I use follow script to gen an ID.
+   * ```javascript
+   * `Math.random()`.slice(2)
+   *
+   * ```
+   * But document.querySelector has such a limitation that I hadn't aware.
+   * querySelector method uses CSS3 selectors for querying the DOM and CSS3 doesn't support ID selectors that start with a digit:
+   * > In CSS, identifiers (including element names, classes, and IDs in selectors) can contain only the characters [a-zA-Z0-9] and ISO 10646 characters U+00A0 and higher, plus the hyphen (-) and the underscore (_); they cannot start with a digit, two hyphens, or a hyphen followed by a digit.
+   *
+   * Use document.getElementByid() is OK. But I think it's a good change to accept the issue and use a real id gen. In here, it's nanoid.
+   */
   default: nanoid(),
 })
 
--- src/utils/copy-to-clipboard.ts
+++ src/utils/copy-to-clipboard.ts
@@ -1,3 +1,14 @@
+/*
+ * Defference Between Copy using Clipboard API(the write API) and document.execCommand('copy')
+ * 1. Clipboard API works async, document.execCommand('copy') works sync.
+ * 2. Clipboard API works on content, e.g. HTML. You can save html anywhere and copy using Clipboard API. But, document.execCommand('copy') indrectly relys on the DOM object. That means you have to pass the ref of the DOM object. It's not easy for FP actually.
+ * 3. Clipboard API works on content, e.g. HTML, that means the content should contain everything. For rich text copy & paste, it's required to apply all styles directly to the HTML so that the copy works. This is a limitation for complex use case. But not a problem for  document.execCommand('copy'). This time, document.execCommand('copy') wins.
+ *
+ * Any way, choose what suits your requirements. In my opinion, Clipboard API looks nices, but for some cases such as rich text copy & paste, it's bad.
+ *
+ * @2021-12-15. Pengson Wang <guyusay@gmail.com>
+ *
+ */
 async function copyToClipboard(editableElement: HTMLElement): Promise<boolean> {
   if (navigator.clipboard) {
     const html = editableElement.innerHTML
